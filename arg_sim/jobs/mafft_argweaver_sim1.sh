9e638095826582e54a7a46fa9391356b90c70a93
Samuel Bogan <snbogan8@gmail.com>
2025-01-19 23:23:54 -0800

    Push simulation to create vcf for relate arg sampling

--- arg_sim/jobs/mafft_argweaver_sim1.sh
+++ arg_sim/jobs/mafft_argweaver_sim1.sh
@@ -10,7 +10,7 @@
 #SBATCH --error=argweaver_sim1.err
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=68GB
 
 # Load dependencies
@@ -20,9 +20,12 @@ conda activate argweaver
 # Move directories
 cd /hb/home/snbogan/pfe
 
+# Align simulated sequences
+mafft --thread 8 simulated_population.fasta > simulated_population_mafft.fasta
+
 # Run argweaver on aligned samples
-arg-sample -f simulated_population.fasta \
+arg-sample -f simulated_population_mafft.fasta \
  -o /hb/home/snbogan/pfe/argweaver_sim1 \
  -N 10000 -m 2.5e-8 -r 1.5e-8 \
- --ntimes 20 --maxtime 100e3 -c 1 -n 100 --overwrite
+ --ntimes 20 --maxtime 100e3 -c 1 -n 100
 
